9e057067575c8dd42eb42d5023ef3ba31d8beb37
999tasneemahmed@gmail.com <999tasneemahmed@gmail.com>
2021-10-27 11:26:10 +0000

    solving error

--- models/staging/staging_food_orders.sql
+++ models/staging/staging_food_orders.sql
@@ -1,6 +1,7 @@
-SELECT 
-   order_id, customer_id,
-   regexp_replace(lower(item_name),'[^a-z]','') as item_name,
-   cast(regexp_replace(item_price,'[^0-9]','') as FLOAT64)  as item_price
-FROM 
-   dataanalysis-1stmeeting.Gad_food_resturant.raw_food_orders;
+select 
+  order_id as order_id,
+  customer_id as customer_number,
+  REGEXP_REPLACE(lower(item_name), '[^a-z]', '') as name,
+  REGEXP_REPLACE(item_price, '[^0-9]', '') as price,
+  date as order_date
+from `dataanalysis-1stmeeting.Gad_food_resturant.raw_food_orders`
